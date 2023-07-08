4ea3dde1ad87304488c1e29f3f65646a0d706666
WanJuKong <juwan3057@naver.com>
2023-07-08 09:12:59 +0900

    7/7

---
+++ script/ajex.js
@@ -0,0 +1,17 @@
+const options = {
+	type : 'post',
+	url : '',
+	dataType : 'json',
+	data : JSON.stringify({}),
+	success : function(result){
+	},
+	error: function(request, status, error) {
+	}
+}
+
+const opt = {
+	"encoding": ,
+	"sampleRateHertz" : 16000,
+	"languaceCode" : "ko-KR",
+	"alternativeLanguageCodes" : [ ],
+
--- script/test.js
+++ script/test.js
@@ -3,23 +3,28 @@ function getMicData(){
 	.then(function(stream){
 		var mediaRecorder = new MediaRecorder(stream);
 		var chunks = [];
+		alert(chunks);
 		mediaRecorder.ondataavailable = function(e){
 			chunks.push(e.data);
 		};
 		mediaRecorder.onstop = function(){
-			var blob = new Blob(chunks, {type: 'audio/mpeg-3'});  //webm
+			alert(chunks);
+			var blob = new Blob(chunks, {type: 'audio/webm'});
 			processAudio(blob);
 		};
 		mediaRecorder.start();
 		setTimeout(function(){
 			mediaRecorder.stop();
 		}, 5000);
-	}).catch(function(error){
-		alert(error);
-	});
+	})
+		.catch(function(error){
+			alert(error);
+		});
 }
 
 function processAudio(audioData){
-	//document.getElementById('id').innerHTML = audioData;
+	var recAudio = document.getElementById('recAudio');
 	recAudio.src=URL.createObjectURL(audioData);
+	alert(typeof(audiodata));
 }
+
